9ffb520950878f77c66540d0630a5d104e670aeb
四条眉毛 <fourfishxiake@gmail.com>
2020-07-06 10:12:21 +0800

    feat: complete the complex ts

--- .babelrc.js
+++ .babelrc.js
@@ -15,5 +15,8 @@ module.exports = {
     ],
     '@babel/preset-typescript',
   ],
-  plugins: ['@babel/plugin-transform-runtime'],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-class-properties',
+  ],
 }
